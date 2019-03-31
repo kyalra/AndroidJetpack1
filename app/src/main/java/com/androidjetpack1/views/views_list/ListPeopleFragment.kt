e418c44ffcaac2b544d472255d76509a4559b097
Rizky Aldi Saputra <rizkyaldi0101@gmail.com>
2019-03-31 16:03:31 +0700

    update

--- app/src/main/java/com/androidjetpack1/views/views_list/ListPeopleFragment.kt
+++ app/src/main/java/com/androidjetpack1/views/views_list/ListPeopleFragment.kt
@@ -2,6 +2,7 @@ package com.androidjetpack1.views.views_list
 
 import android.app.Fragment
 import android.arch.lifecycle.Observer
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -15,15 +16,15 @@ import com.androidjetpack1.views.views_add.AddPeopleData
 import com.androidjetpack1.views.views_details.DetailAct
 import kotlinx.android.synthetic.main.fragment_for_list_people.*
 
-class ListPeopleFragment:Fragment(),ListPeopleAdapter.OnItemClickListener {
+class ListPeopleFragment:android.support.v4.app.Fragment(),ListPeopleAdapter.OnItemClickListener {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.fragment_for_list_people, container, false)
     }
 
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         addFab.setOnClickListener {
-           val intent=Intent(activity.applicationContext,AddPeopleData::class.java)
+           val intent=Intent(context,AddPeopleData::class.java)
             startActivity(intent)
         }
     }
@@ -31,10 +32,10 @@ class ListPeopleFragment:Fragment(),ListPeopleAdapter.OnItemClickListener {
 
     override fun onItemClick(people: People, ItemView: View) {
         Toast.makeText(
-            activity.applicationContext, "Test Click Contact List",
+            context, "Test Click Contact List",
             Toast.LENGTH_SHORT
         ).show()
-        val detailIntent=Intent(activity.applicationContext,DetailAct::class.java)
+        val detailIntent=Intent(context,DetailAct::class.java)
         detailIntent.putExtra(getString(R.string.people_id),people.id)
         startActivity(detailIntent)
 
